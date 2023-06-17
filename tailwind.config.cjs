01501c392656ec9ed0facc715908c8caf7e52c2e
binhthaitrinh <trinhthaibinh.ecom@gmail.com>
2023-06-16 21:08:35 -0700

    Add phone number and zalo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,31 @@ module.exports = {
       borderWidth: {
         1: '1px',
       },
+      keyframes: {
+        wiggle: {
+          '0%': {
+            transform: 'rotate(0) scale(1) skew(1deg)',
+          },
+          '10%': {
+            transform: 'rotate(-25deg) scale(1) skew(1deg)',
+          },
+          '20%': {
+            transform: 'rotate(25deg) scale(1) skew(1deg)',
+          },
+          '30%': {
+            transform: 'rotate(-25deg) scale(1) skew(1deg)',
+          },
+          '40%': {
+            transform: 'rotate(25deg) scale(1) skew(1deg)',
+          },
+          '50%': {
+            transform: 'rotate(0) scale(1) skew(1deg)',
+          },
+          '100%': {
+            transform: 'rotate(0) scale(1) skew(1deg)',
+          },
+        },
+      },
     },
   },
   plugins: [],
